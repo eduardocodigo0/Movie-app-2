5d146756d25898d75d7178a2eb6d7fcd65162cb1
eduardocodigo0 <eduardocodigo0@gmail.com>
2022-04-14 21:20:54 -0300

    Dependencias para ocultar a API key

--- app/src/main/java/com/solinftec/mobile/movieapp2/MainActivity.kt
+++ app/src/main/java/com/solinftec/mobile/movieapp2/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.solinftec.mobile.movieapp2
 
+import android.content.Context
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
