9d5a2cd30d53d2b3744073f8351729562d7f3e87
eduardocodigo0 <eduardocodigo0@gmail.com>
2022-04-15 10:50:11 -0300

    Permissao de acesso a internet remoçao de logger do koin

--- app/src/main/java/com/eduardocodigo0/mobile/movieapp2/di/KoinModules.kt
+++ app/src/main/java/com/eduardocodigo0/mobile/movieapp2/di/KoinModules.kt
@@ -3,6 +3,7 @@ package com.eduardocodigo0.mobile.movieapp2.di
 import com.eduardocodigo0.mobile.movieapp2.network.provideMovieAPI
 import com.eduardocodigo0.mobile.movieapp2.network.provideRetrofit
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 
 val networkModules = module {
