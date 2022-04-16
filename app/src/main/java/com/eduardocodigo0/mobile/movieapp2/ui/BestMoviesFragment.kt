ef3df95e6204d412f1675822e22eb43f66704237
eduardocodigo0 <eduardocodigo0@gmail.com>
2022-04-15 22:00:06 -0300

    Item da lista de filmes

--- app/src/main/java/com/eduardocodigo0/mobile/movieapp2/ui/BestMoviesFragment.kt
+++ app/src/main/java/com/eduardocodigo0/mobile/movieapp2/ui/BestMoviesFragment.kt
@@ -6,17 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.eduardocodigo0.mobile.movieapp2.R
+import com.eduardocodigo0.mobile.movieapp2.databinding.FragmentBestMoviesBinding
+import com.eduardocodigo0.mobile.movieapp2.databinding.FragmentSplashScreenBinding
 
 
 class BestMoviesFragment : Fragment() {
 
+    private var _binding: FragmentBestMoviesBinding? = null
+    private  val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_best_movies, container, false)
+    ): View {
+        _binding = FragmentBestMoviesBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
